43610aacd2cf3029bf04c64776ddba273f6a3c20
Vinicius Cheretti Copola <141167117+vinicopola@users.noreply.github.com>
2025-04-08 13:42:02 -0300

    Add files via upload

---
+++ src/JS/index.js
@@ -0,0 +1,16 @@
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        // passo 3 - verificar se já existe um botão selecionado, se sim, devemos remover a seleção dele 
+        const botaoSelecionado = document.querySelector(".botao.selecionado");
+        botaoSelecionado.classList.remove("selecionado");
+
+        botao.classList.add("selecionado");
+
+        const personagemSelecionado = document.querySelector(".personagem.selecionado");
+        personagemSelecionado.classList.remove("selecionado");
+        personagens[indice].classList.add("selecionado");
+    });
+});
